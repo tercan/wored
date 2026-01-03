3bbaa7047eba2ddf14d3d216e2878bc5bb6c293b
Tercan Keskin <tercankeskin@gmail.com>
2026-01-03 13:09:09 +0300

    Initial Commit

---
+++ wored/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wored
+//
+//  Created by Tercan Keskin on 3.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
